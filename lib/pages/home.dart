44bc3d5292f1a994a4266ff7a5f59a4cca083262
Mukunzijames <mukunzindahiro@gmail.com>
2024-10-21 10:40:34 +0200

    widgets

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -17,14 +18,40 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0.0,
         centerTitle: true,
-        leading: Container(
+        leading: GestureDetector(
+          child: Container(
           margin: EdgeInsets.all(10),
-           decoration: BoxDecoration(
-            color: Colors.black,
+          alignment: Alignment.center,
+          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg',
+          width: 20,
+          height: 20,
+          ),
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
             borderRadius: BorderRadius.circular(10),
           )
+          ),
         ),
-        
+        actions: [
+          GestureDetector(
+            onTap: () {
+
+            },
+            child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            child: SvgPicture.asset('assets/icons/dots.svg',
+            width: 5,
+            height: 5,
+            ),
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          )
+        ]
       )
     );
   }
